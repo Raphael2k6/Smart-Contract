76c3f50ff6a57c47763868cc5d23a45c1ce3d700
raphaeldonanu <raphael.donanu@konga.com>
2021-12-20 13:09:05 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React, { useEffect, useState, useCallback } from 'react';
 import './App.css';
-
+import { ethers } from "ethers";
+import { abi } from './abiData';
 function App() {
+
+  const [contractName, setContractName] = useState("");
+  const [contractSupply, setContractSupply] = useState("");
+
+
+  var url = 'https://mainnet.infura.io/v3/b4e232ec346a4f7aada16c229c5e3218'
+  let contractAddress = "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0";
+  const provider = new ethers.providers.getDefaultProvider(url);
+  const contract = new ethers.Contract(contractAddress, abi, provider);
+
+  const getInfo2 = useCallback(
+    async () => {
+      const name = await contract.name()
+      setContractName(name);
+      console.log(name, 'name');
+
+      const totalSupply = await contract.totalSupply()
+
+      const formatedSupply = ethers.utils.formatEther(totalSupply);
+      console.log(formatedSupply, 'totalSupply');
+      setContractSupply(formatedSupply);
+    }, [])
+
+  useEffect(() => {
+    getInfo2()
+  }, [getInfo2])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div class="h-100 row align-items-center m-5">
+          <div className="card text-white bg-secondary m-5">
+            <div className="card-body">
+              <p className="card-text text-center">Contract Name: {contractName}</p>
+              <p className="card-text text-center">Contract Supply: {contractSupply}</p>
+            </div>
+        </div>
+      </div>
     </div>
+
   );
 }
 
